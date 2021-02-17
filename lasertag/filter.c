5c2ee8d06f8fdbb337a9e8f534c492680b417d0f
dnybodan <dnybo.dan@gmail.com>
2021-02-16 18:15:51 -0700

    Changes made 4.16

--- lasertag/filter.c
+++ lasertag/filter.c
@@ -247,6 +247,7 @@ double filter_firFilter() {
     // FIR filter
     queue_overwritePush(&yQueue, queue_readElementAt(&xQueue, i));
   }
+  return tempZ;
 }
 
 // Use this to invoke a single iir filter. Input comes from yQueue.
@@ -279,7 +280,7 @@ double filter_iirFilter(uint16_t filterNumber) {
     queue_overwritePush(&outputQueue[filterNumber], tempZ);
     queue_overwritePush(&zQueue[filterNumber], tempZ);
   }
-  // return &zQueue.data;
+  return tempZ;
 }
 
 // Use this to compute the power for values contained in an outputQueue.
@@ -305,6 +306,7 @@ double filter_computePower(uint16_t filterNumber, bool forceComputeFromScratch,
            queue_readElementAt(&outputQueue[filterNumber], i);
   }
   currentPowerValue[filterNumber] = sum;
+  return sum;
 }
 
 // Returns the last-computed output power value for the IIR filter
